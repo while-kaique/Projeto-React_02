fbe6ce56565f19f82227587faf19c24fe47bf259
while-kaique <123902283+while-kaique@users.noreply.github.com>
2024-01-10 02:14:06 -0300

    Área e página de pesquisa
    Começando a área de pesquisa de filmes

--- movies_lib/src/components/Navbar.jsx
+++ movies_lib/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
 
 import './Navbar.css'
 
 function Navbar() {
+
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit= (e) => {
+        e.preventDefault()
+        
+        if (!search) return
+
+        navigate(`/search?q=${search}`)
+        setSearch('')
+    }
+
     return(
         <div>
             <nav id='navbar'>
@@ -13,8 +28,10 @@ function Navbar() {
                     Movies Lib
                 </Link>
             </h2>
-                <form >
-                    <input type="text" placeholder="Busque um filme" />
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Busque um filme"
+                    onChange={(e=>setSearch(e.target.value))}
+                    value={search} />
                     <button type="submit">
                         <BiSearchAlt2 />
                     </button>
